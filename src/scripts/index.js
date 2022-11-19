b2b97f7ff3202698e88ec05c3e287530389b6cfe
IhratPramudya <pramudyaihrat@gmail.com>
2022-11-19 22:10:57 +0700

    Add Project To Github Application

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-duplicates */
+// Import our custom CSS
+import '../scss/styles.scss';
+import '../styles/main.css';
+import 'bootstrap';
+
+// Import all of Bootstrap's JS
+// import * as bootstrap from 'bootstrap';

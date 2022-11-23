70a709c14e1bdb364f2d8a2ec054e91d1498c80f
Rizal Suryana <izalsuryana@gmail.com>
2022-11-23 20:19:31 +0700

    add js belum selsai untuk drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,16 @@
 /* eslint-disable import/no-duplicates */
 // Import our custom CSS
+import 'regenerator-runtime';
 import '../styles/main.css';
 import '../styles/responsive.css';
 import 'bootstrap';
+import App from './views/app';
+
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navDrawer'),
+  content: document.querySelector('#mainContent'),
+});
 
 // Import all of Bootstrap's JS
 // import * as bootstrap from 'bootstrap';

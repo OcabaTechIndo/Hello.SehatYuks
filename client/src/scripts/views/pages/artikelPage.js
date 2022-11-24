707834f650db3b255135d6f3d262eed6df4b66e1
Understrike123 <muhilhamfebr2@gmail.com>
2022-11-24 19:29:20 +0700

    membuat routes dan membuat custom element artrikel element

---
+++ client/src/scripts/views/pages/artikelPage.js
@@ -0,0 +1,15 @@
+import '../templates/templateArtikel';
+
+const ArtikelPage = {
+    async render() {
+        return `
+            <artikel-container></artikel-container>
+        `;
+    },
+
+    /* kontent render akan dipakai setelah API siap digunakan
+    async afterRender() {
+        const elementContentArtikel = document.querySelector('artikel-container');
+    }
+    */
+}

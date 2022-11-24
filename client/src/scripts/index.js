d487d43dea240a646db01b06b9aadffa3168051f
IhratPramudya <pramudyaihrat@gmail.com>
2022-11-24 15:19:51 +0700

    Merubah Struktur kode antara client dan server

---
+++ client/src/scripts/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-duplicates */
+// Import our custom CSS
+import 'regenerator-runtime';
+import '../scss/styles.scss';
+import '../styles/main.css';
+import '../styles/responsive.css';
+import 'bootstrap';
+import App from './views/app';
+
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navDrawer'),
+  content: document.querySelector('#mainContent'),
+});
+
+// Import all of Bootstrap's JS
+// import * as bootstrap from 'bootstrap';

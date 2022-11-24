806fbc3966dd20b78460461c01cd8cd1bbacb7ce
IhratPramudya <pramudyaihrat@gmail.com>
2022-11-25 01:29:20 +0700

    Menambahkan Routs

--- server/dbService/dataBaseDrug.js
+++ server/dbService/dataBaseDrug.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const dotenv = require('dotenv');
+let instance = null;
 dotenv.config();
-
 
 const connection = mysql.createConnection({
     host: process.env.HOST,
@@ -17,3 +17,55 @@ connection.connect((err) => {
     }
     console.log('db ' + connection.state);
 });
+
+
+class DbDrug {
+    static getInstaceDrug () {
+        return instance ? instance : new DbDrug();
+    }
+
+
+    async insertDrugData(DrugData) {
+        try {
+            const { background, brand_name, name_general, numberdrugaccess, summary } = DrugData;
+            const insertId = await new Promise((resolve, reject) => {
+                const query = "INSERT INTO drug_table (background, brand_name, name_general, numberdrugaccess, summary) VALUES (?,?,?,?,?);";
+
+                connection.query(query, [background, brand_name, name_general, numberdrugaccess, summary] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.insertId);
+                })
+            });
+
+            return {
+                id: insertId,
+                background: background,
+                brand_name: brand_name,
+                name_general: name_general,
+                numberdrugaccess: numberdrugaccess,
+                summary: summary
+            }
+        }catch(err) {
+            console.log(err.message)
+        }
+    }
+
+
+    async getAllDrug() {
+        try{
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM drug_table";
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                })
+            });
+            return response;
+        } catch(err) {
+            console.log(err.message);
+        }
+    }
+}
+
+module.exports = DbDrug;
--- server/src/server.js
+++ server/src/server.js
@@ -1,29 +1,49 @@
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
 const dotenv = require('dotenv');
+dotenv.config();
 
-dotenv.config()
-
-// const dbSevice = require('../dbService/dataBaseDrug');
+// const dbService = require('./dbService');
+const DbDrug = require('../dbService/dataBaseDrug');
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended : false }));
 
 
-// app.post('/obat/add', (request, response) => {
-//     const { background, brand_name, name_general, numberdrugaccess,  summary } = request.body;
+app.post('/obat/add', (request, response) => {
+    const { background, brand_name, name_general, numberdrugaccess, summary } = request.body;
+    const DrugData = {
+        background,
+        brand_name,
+        name_general,
+        numberdrugaccess,
+        summary
+    }
+    console.log(DrugData);
+    const dataBase = DbDrug.getInstaceDrug();
+    const result = dataBase.insertDrugData(DrugData);
 
-//     const dataBase = dbSevice.
-// })
+    result
+    .then(data => response.json({ data: data }))
+    .catch(err => console.log(err.message))
+})
+
+
+
+app.get('/obat/list', (request, response) => {
+    const dataBase = DbDrug.getInstaceDrug();
+    const result = dataBase.getAllDrug();
+
+    result
+    .then(data => response.json({data: data}))
+    .catch( err => console.log(err.message))
+})
 
 
 app.listen(process.env.PORT, () => {
-    console.log(`server running}`)
+    console.log(`server running`)
 })
 
 

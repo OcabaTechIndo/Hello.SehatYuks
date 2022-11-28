6502fc711432f2705315f713979fc91c63b5e019
Understrike123 <muhilhamfebr2@gmail.com>
2022-11-28 21:53:53 +0700

    nambah detail obat

--- client/src/scripts/views/templates/obat_template.js
+++ client/src/scripts/views/templates/obat_template.js
@@ -8,27 +8,29 @@ class Obat extends HTMLElement {
     render() {
         this.innerHTML = `
             <div id="list_obat" class="list_container">
-                <h3>Home > Artikel</h3>
+                <h3>Home > Info obat</h3>
                 <img src="./logo/obat_heart.png" alt="">
                 <p>Masih ragu cara memilih obat yang tepat? jangan khawatir, IRIS akan membantu kamu</p>
                 <div id="searchObat" class"obatSearchContainer"></div>
-                <div id="drop_list" class="list_content d-flex"></div>    
+                <div id="drop_list" class="list_content d-flex">
+                    <div id="obat_card_list"></div>
+                </div>
             </div>
         `;
-        const elementListObat = this.querySelector('#drop_list');
+        const elementListObat = this.querySelector('#obat_card_list');
         elementListObat.innerHTML = '';
         this._obatData.forEach((obat) => {
             elementListObat.innerHTML += `
             <div class="card" style="width: 18rem;">
             <img src="" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${obat.background}</h5>
-                    <p class="card-text">${obat.brand_name}</p>
+                    <h5 class="card-title">${obat.background.slice(0, 20)}</h5>
+                    <p class="card-text">${obat.brand_name.slice(0, 10)}</p>
                     <p class="card-text">${obat.id}</p>
-                    <p class="card-text">${obat.indicator}</p>
+                    <p class="card-text">${obat.indicator.slice(0, 20)}</p>
                     <p class="card-text">${obat.name_general}</p>
                     <p class="card-text">${obat.numberdrugaccess}</p>
-                    <p class="card-text">${obat.summary}</p>
+                    <p class="card-text">${obat.summary.slice(0, 20)}</p>
                     <a href="#" class="btn btn-primary">Baca</a>
                 </div>
             </div>
---
+++ client/src/scripts/views/templates/template-detail-obat.js
@@ -0,0 +1,19 @@
+import GLOBAL_VAR from "../../globals/golbalVariabel";
+
+
+const templateDetailObat = (obat) => `
+<div class="card" class="col" style="width: 18rem;">
+<img src="" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${obat.background}</h5>
+        <p class="card-text">${obat.brand_name}</p>
+        <p class="card-text">${obat.id}</p>
+        <p class="card-text">${obat.indicator}</p>
+        <p class="card-text">${obat.name_general}</p>
+        <p class="card-text">${obat.numberdrugaccess}</p>
+        <p class="card-text">${obat.summary}</p>
+    </div>
+</div>
+`;
+
+export default templateDetailObat;

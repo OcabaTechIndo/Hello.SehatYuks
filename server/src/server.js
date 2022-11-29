e47dfe515349a36f920909ee9ba3d8d76b93db72
IhratPramudya <pramudyaihrat@gmail.com>
2022-11-29 20:24:24 +0700

    add User Authentication user

--- server/src/server.js
+++ server/src/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 require ('./routes/DrugRoute')(app)
-require('./controlers/user.router')(app)
+require('./endpoint/user.router')(app)
 
 app.listen(process.env.PORT, () => {
     console.log(`server running`)

b96aae20dfbce70281f3ff64fdef7629e9cd55c4
IhratPramudya <pramudyaihrat@gmail.com>
2022-11-30 01:40:11 +0700

    Bug

--- server/src/endpoint/drug.router.js
+++ server/src/endpoint/drug.router.js
@@ -3,6 +3,6 @@ const { createDrug, getAllDrug, gedDrugByid, } = require('../routes/DrugRoute')
 
 module.exports = function(app){
     app.post('/obat/add', checkToken,createDrug);
-    app.post('/obat/list', checkToken, getAllDrug);
+    app.get('/obat/list', checkToken, getAllDrug);
     app.get('/obat/detail/:id', checkToken, gedDrugByid);
 }

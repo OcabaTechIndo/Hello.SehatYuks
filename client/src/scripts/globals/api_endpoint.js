291cc961da82c862ffa1853d74d17218e0a93b82
IhratPramudya <pramudyaihrat@gmail.com>
2022-11-30 20:44:23 +0700

    spinner add

--- client/src/scripts/globals/api_endpoint.js
+++ client/src/scripts/globals/api_endpoint.js
@@ -1,7 +1,8 @@
 import GLOBAL_VAR from './golbalVariabel';
 
 const API_ENDPOINT = {
-    OBAT_LIST: `${GLOBAL_VAR.URL}/obat/list`,
+    OBAT_LIST: `${GLOBAL_VAR.URL}obat/list`,
+    LOGIN_USER: `${GLOBAL_VAR.URL}user/login`,
 };
 
 export default API_ENDPOINT;

87c744111ae8697e2597e8b05118df0e5740e859
Understrike123 <muhilhamfebr2@gmail.com>
2022-11-30 17:38:35 +0700

    nambah data API

--- client/src/scripts/data/dataAPI.js
+++ client/src/scripts/data/dataAPI.js
@@ -1,8 +1,14 @@
+/* eslint-disable quote-props */
 // import API_ENDPOINT from '../globals/api_endpoint';
 
 class DataAPI {
     static async GET_obat() {
-        const response = await fetch('http://localhost:9090/obat/list');
+        const response = await fetch('http://localhost:9090/obat/list', {
+            headers: {
+                Accept: 'application/json',
+                Authentication: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyMSwiZmlyc3ROYW1lIjoidGVzdCIsImxhc3ROYW1lIjoidGVzdCIsImdlbmRlciI6InRlc3QiLCJlbWFpbCI6InRlc3QiLCJudW1iZXIiOiIxMzM0MzI0NCJ9LCJpYXQiOjE2Njk3NDcwMzUsImV4cCI6MTY2OTc1MDYzNX0.Odd2n_GolgKQVRsg1ai8ngvM3HUrzLk0IdulsebNuLw',
+            },
+        });
         const responseJSON = await response.json();
         return responseJSON.obat;
     }

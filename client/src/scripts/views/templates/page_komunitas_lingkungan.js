0ca91c1d51cf73a07f7dd004567faf7ca2e5f91f
Understrike123 <muhilhamfebr2@gmail.com>
2022-12-10 21:08:11 +0700

    benerin dikit komunitas lingkungan

--- client/src/scripts/views/templates/page_komunitas_lingkungan.js
+++ client/src/scripts/views/templates/page_komunitas_lingkungan.js
@@ -14,27 +14,27 @@ class LingkunganTemplate extends HTMLElement {
 
     render() {
         this.innerHTML = `
-    <div class="container px-4 p-5">
-    <div hidden id="spinner"></div>
-        <div class="text-center">
-            <h2>Hallo Selamat Datang di Komunitas Lingkungan</h2>
-    </div>
-    <div class="row p-5">
-        <div class="col-8">
-            <div class="p-3 border bg-light">
-            <form>
-                <div class="mb-3">
-                    <label for="answer" class="form-label">Masukan Serta Categori Pertanyaan</label>
-                    <textarea class="form-control" id="answer" placeholder="Masukkan Pertanyaan" required rows="3"></textarea>
+            <div class="container px-4 p-5">
+                <div hidden id="spinner"></div>
+                    <div class="text-center">
+                        <h2>Hallo Selamat Datang di Komunitas Lingkungan</h2>
                 </div>
-                <button type="submit" class="btn btn-outline-info" id="buttonElementAnswer"><i class="fa-solid fa-plus"></i></button>
-                </form>
+                <div class="row p-5">
+                    <div class="col-8">
+                        <div class="p-3 border bg-light">
+                        <form>
+                            <div class="mb-3">
+                                <label for="answer" class="form-label">Masukan Serta Categori Pertanyaan</label>
+                                <textarea class="form-control" id="answer" placeholder="Masukkan Pertanyaan" required rows="3"></textarea>
+                            </div>
+                            <button class="btn btn-outline-info" id="buttonElementAnswer"><i class="fa-solid fa-plus"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div>
+                </div>
             </div>
-        </div>
-        <div class="col-4">
-            <div>
-        </div>
-    </div>
         `;
     }
 }

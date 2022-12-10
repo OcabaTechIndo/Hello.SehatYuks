f2111c8424a905229b933eb199e8be080b424e2f
IhratPramudya <pramudyaihrat@gmail.com>
2022-12-11 04:46:11 +0700

    add Configuration deploy Back End

--- client/src/scripts/globals/golbalVariabel.js
+++ client/src/scripts/globals/golbalVariabel.js
@@ -1,5 +1,5 @@
 const GLOBAL_VAR = {
-    URL: 'http://54.185.109.161:9090/',
+    URL: 'http://34.222.35.64:9090/',
     DATABASE_NAME: 'sehatyuks',
     OBJECT_STORE_NAME: 'user_login',
     DATABASE_VERSION: '1',

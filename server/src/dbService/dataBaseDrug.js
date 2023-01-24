6d2256ba48a0103d35ac87adb525b111dc823668
IhratPramudya <pramudyaihrat@gmail.com>
2023-01-24 22:01:49 +0700

    deploy update

--- server/src/dbService/dataBaseDrug.js
+++ server/src/dbService/dataBaseDrug.js
@@ -1,19 +1,17 @@
-const mysql = require('mysql');
-
+const mysql = require('mysql2');
+require('dotenv').config();
  const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'ocabatechindo',
-    password: 'sehatyuks',
-    database: 'sehatyuks',
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     port: 3306
 })
 
 connection.connect((err) => {
-    if (err) {
-        console.log(err.message);
-    }
-    console.log('db ' + connection.state);
-});
+    if (err) throw err;
+    console.log('Connected!');
+  });
 
 module.exports = connection;
 

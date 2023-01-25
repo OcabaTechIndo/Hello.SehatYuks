b93ddd5bd1ba3fab34f3e9f2ca343adf0eb428b0
IhratPramudya <pramudyaihrat@gmail.com>
2023-01-26 02:21:26 +0700

    deploy

--- client/src/scripts/globals/golbalVariabel.js
+++ client/src/scripts/globals/golbalVariabel.js
@@ -1,5 +1,5 @@
 const GLOBAL_VAR = {
-    URL: 'http://34.222.35.64:9090/',
+    URL: 'https://sehatyuks-production.up.railway.app/',
     DATABASE_NAME: 'sehatyuks',
     OBJECT_STORE_NAME: 'user_login',
     DATABASE_VERSION: '1',
